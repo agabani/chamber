033ffdbd740afc1e2f039033a1ecc60b6edc18a2
agabani <agabani@users.noreply.github.com>
2023-10-13 21:32:55 +0100

    feat: add experimental api

--- src/distribution/mod.rs
+++ src/distribution/mod.rs
@@ -1,2 +1,4 @@
-/// A basic client for interacting with the container distribution API
+/// API helpers for structured interaction with container distribution API.
+pub mod api;
+/// A basic client for interacting with the container distribution API.
 pub mod client;
--- tests/distribution.rs
+++ tests/distribution.rs
@@ -1,4 +1,7 @@
-use chamber::distribution::client::Client;
+use chamber::distribution::{
+    api::{SupportAPI, SupportRequest},
+    client::Client,
+};
 use hyper::{Body, Request};
 
 const BASE_URL: &str = "http://localhost:5000";
@@ -15,6 +18,20 @@ async fn v2_returns_200() {
 
     // Act
     let response = client.send(request).await.unwrap();
+
+    // Assert
+    assert_eq!(hyper::http::StatusCode::OK, response.status());
+}
+
+#[tokio::test]
+async fn v2_returns_unit() {
+    // Arrange
+    let client = Client::new(hyper::Client::new());
+    let api = SupportAPI::new(client);
+    let request = SupportRequest::new().base_url(BASE_URL);
+
+    // Act
+    let response = api.check(request).await.unwrap();
 
     // Assert
     assert_eq!(hyper::http::StatusCode::OK, response.status());
