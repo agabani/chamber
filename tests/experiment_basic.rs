29bbacc97a6bf46843f0926b5453e8ffa006ca7d
agabani <agabani@users.noreply.github.com>
2023-10-14 21:37:33 +0100

    feat: add authentication experiment

---
+++ tests/experiment_basic.rs
@@ -0,0 +1,44 @@
+use chamber::distribution::client::Client;
+use hyper::{Body, Method, Request, StatusCode};
+
+const BASE_URL: &str = "http://localhost:5001";
+
+#[tokio::test]
+async fn workflow() {
+    // Arrange
+    let client = Client::new(hyper::Client::new());
+
+    // Act
+    let request = Request::builder()
+        .method(Method::GET)
+        .uri(format!("{BASE_URL}/v2/"))
+        .body(Body::empty())
+        .unwrap();
+
+    let response = client.send(request).await.unwrap();
+
+    // Assert
+    assert_eq!(response.status(), StatusCode::UNAUTHORIZED);
+    assert_eq!(
+        response
+            .headers()
+            .get("Www-Authenticate")
+            .unwrap()
+            .to_str()
+            .unwrap(),
+        "Basic realm=\"Registry Realm\""
+    );
+
+    // Act
+    let request = Request::builder()
+        .method(Method::GET)
+        .uri(format!("{BASE_URL}/v2/"))
+        .header("Authorization", "Basic YWRtaW46cGFzc3dvcmQ=")
+        .body(Body::empty())
+        .unwrap();
+
+    let response = client.send(request).await.unwrap();
+
+    // Assert
+    assert_eq!(response.status(), StatusCode::OK);
+}
