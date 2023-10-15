59f230acfceec5af69bce920adc49041fe9ebe0b
agabani <agabani@users.noreply.github.com>
2023-10-15 16:59:33 +0100

    feat: www parser

--- src/parser/www_authenticate.rs
+++ src/parser/www_authenticate.rs
@@ -8,35 +8,35 @@ use nom::{
     IResult,
 };
 
-///
+/// WWW-Authenticate response header.
 #[derive(Debug)]
 pub struct WwwAuthenticate<'a> {
-    ///
+    /// Challenges.
     pub challenges: Vec<Challenge<'a>>,
 }
 
-///
+/// Challenge.
 #[derive(Debug)]
 pub struct Challenge<'a> {
-    ///
+    /// Authentication scheme.
     pub auth_scheme: &'a str,
-    ///
+    /// Authentication parameters.
     pub auth_params: Vec<AuthParam<'a>>,
 }
 
-///
+/// Authentication parameter.
 #[derive(Debug)]
 pub struct AuthParam<'a> {
-    ///
+    /// Key.
     pub key: &'a str,
-    ///
+    /// Value.
     pub value: &'a str,
 }
 
 impl<'a> WwwAuthenticate<'a> {
-    ///
+    /// Parse a [`WwwAuthenticate`] from a string.
     pub fn parse(input: &'a str) -> Result<Self, nom::Err<nom::error::Error<&'a str>>> {
-        let (_remaining, challenges) = all_consuming(challenges())(input)?;
+        let (_, challenges) = all_consuming(challenges())(input)?;
 
         Ok(Self {
             challenges: challenges
@@ -97,92 +97,157 @@ mod tests {
 
     #[test]
     fn basic() {
+        // Arrange
         let input = r#"Basic realm="Registry Realm""#;
 
+        // Act
         let www_authenticate = WwwAuthenticate::parse(input).unwrap();
 
+        // Assert
         println!("{www_authenticate:?}");
     }
 
     #[test]
     fn bearer() {
+        // Arrange
         let input = r#"Bearer realm="http://127.0.0.1:5003/auth",service="Docker registry",error="invalid_token""#;
 
+        // Act
         let www_authenticate = WwwAuthenticate::parse(input).unwrap();
 
+        // Assert
         println!("{www_authenticate:?}");
     }
 
     #[test]
-    fn rfc() {
+    fn rfc_7235() {
+        // Arrange
         let input =
             r#"Newauth realm="apps", type=1, title="Login to \"apps\"", Basic realm="simple""#;
 
+        // Act
         let www_authenticate = WwwAuthenticate::parse(input).unwrap();
 
+        // Assert
         println!("{www_authenticate:?}");
     }
 
     #[test]
     fn test_auth_param() {
+        // Arrange
         let input = "key=value";
 
+        // Act
         let (remaining, (key, value)) = auth_param()(input).unwrap();
 
-        println!("remaining:{remaining} key:{key} value:{value}");
+        // Assert
+        assert_eq!(remaining, "");
+        assert_eq!(key, "key");
+        assert_eq!(value, "value");
     }
 
     #[test]
     fn test_auth_params() {
+        // Arrange
         let input =
             r#"realm="http://127.0.0.1:5003/auth",service="Docker registry",error="invalid_token""#;
 
+        // Act
         let (remaining, params) = auth_params()(input).unwrap();
 
-        println!("remaining:{remaining} params:{params:?}");
+        // Assert
+        assert_eq!(remaining, "");
+        assert_eq!(params.len(), 3);
+        assert_eq!(params[0].0, "realm");
+        assert_eq!(params[0].1, "http://127.0.0.1:5003/auth");
+        assert_eq!(params[1].0, "service");
+        assert_eq!(params[1].1, "Docker registry");
+        assert_eq!(params[2].0, "error");
+        assert_eq!(params[2].1, "invalid_token");
     }
 
     #[test]
     fn test_challenge() {
+        // Arrange
         let input = r#"Bearer realm="http://127.0.0.1:5003/auth",service="Docker registry",error="invalid_token""#;
 
+        // Act
         let (remaining, challenge) = challenge()(input).unwrap();
 
-        println!("remaining:{remaining} challenge:{challenge:?}");
+        // Assert
+        assert_eq!(remaining, "");
+        assert_eq!(challenge.0, "Bearer");
+        assert_eq!(challenge.1.len(), 3);
+        assert_eq!(challenge.1[0].0, "realm");
+        assert_eq!(challenge.1[0].1, "http://127.0.0.1:5003/auth");
+        assert_eq!(challenge.1[1].0, "service");
+        assert_eq!(challenge.1[1].1, "Docker registry");
+        assert_eq!(challenge.1[2].0, "error");
+        assert_eq!(challenge.1[2].1, "invalid_token");
     }
 
     #[test]
     fn test_challenges() {
-        let input =
+        // Arrange
+        let input: &str =
             r#"Newauth realm="apps", type=1, title="Login to \"apps\"", Basic realm="simple""#;
 
+        // Act
         let (remaining, challenges) = challenges()(input).unwrap();
 
-        println!("remaining:{remaining} challenge:{challenges:?}");
+        // Assert
+        assert_eq!(remaining, "");
+        assert_eq!(challenges.len(), 2);
+        assert_eq!(challenges[0].0, "Newauth");
+        assert_eq!(challenges[0].1.len(), 3);
+        assert_eq!(challenges[0].1[0].0, "realm");
+        assert_eq!(challenges[0].1[0].1, "apps");
+        assert_eq!(challenges[0].1[1].0, "type");
+        assert_eq!(challenges[0].1[1].1, "1");
+        assert_eq!(challenges[0].1[2].0, "title");
+        assert_eq!(challenges[0].1[2].1, "Login to \\\"apps\\\"");
+        assert_eq!(challenges[1].0, "Basic");
+        assert_eq!(challenges[1].1.len(), 1);
+        assert_eq!(challenges[1].1[0].0, "realm");
+        assert_eq!(challenges[1].1[0].1, "simple");
     }
 
     #[test]
     fn test_quoted_string() {
+        // Arrange
         let input = r#""Docker registry""#;
 
+        // Act
         let (remaining, string) = quoted_string()(input).unwrap();
 
-        println!("remaining:{remaining} string:{string:?}");
+        // Assert
+        assert_eq!(remaining, "");
+        assert_eq!(string, "Docker registry");
     }
 
     #[test]
     fn test_quoted_string_escaped() {
+        // Arrange
         let input = r#""Login to \"apps\"""#;
 
+        // Act
         let (remaining, string) = quoted_string()(input).unwrap();
 
-        println!("remaining:{remaining} string:{string:?}");
+        // Assert
+        assert_eq!(remaining, "");
+        assert_eq!(string, "Login to \\\"apps\\\"");
     }
 
     #[test]
     fn test_token() {
-        let (remaining, value) = token()("liistx").unwrap();
+        // Arrange
+        let input = "list";
 
-        println!("remaining:{remaining} value:{value}");
+        // Act
+        let (remaining, string) = token()(input).unwrap();
+
+        // Assert
+        assert_eq!(remaining, "");
+        assert_eq!(string, "list");
     }
 }
