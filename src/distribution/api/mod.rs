5c45c28926e07e9e35aa320f0d9294e411670cfd
agabani <agabani@users.noreply.github.com>
2023-10-17 19:33:07 +0100

    feat: clean state

--- src/distribution/api/mod.rs
+++
@@ -1,9 +0,0 @@
-mod catalog;
-mod error;
-mod support;
-mod tags_list;
-
-pub use catalog::*;
-pub use error::*;
-pub use support::*;
-pub use tags_list::*;
--- src/distribution/service/mod.rs
+++
@@ -1,7 +0,0 @@
-mod authentication;
-mod support;
-
-#[allow(clippy::module_name_repetitions)]
-pub use authentication::{AuthenticationLayer, AuthenticationService};
-#[allow(clippy::module_name_repetitions)]
-pub use support::{SupportLayer, SupportRequest, SupportResponse, SupportService};
