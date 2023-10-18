cff19308cdd765181f0ea299c3d298405ef2c4f7
agabani <agabani@users.noreply.github.com>
2023-10-18 23:04:24 +0100

    feat: distribution contract

--- src/distribution/api/v2/manifests_get.rs
+++ src/distribution/api/v2/manifests_get.rs
@@ -4,6 +4,7 @@ use crate::distribution::{
     self,
     authentication::{Authentication, Credential},
     error,
+    www_authenticate::WwwAuthenticate,
 };
 
 /// application/vnd.docker.distribution.manifest.v1+json
@@ -24,12 +25,12 @@ pub const APPLICATION_VND_DOCKER_DISTRIBUTION_MANIFEST_LIST_V2_JSON: &'static st
 
 ///
 pub struct Request {
-    _authentication: Option<Authentication>,
-    _base_url: url::Url,
-    _credential: Option<Credential>,
-    _name: String,
-    _reference: String,
-    _accept: Vec<String>,
+    authentication: Option<Authentication>,
+    base_url: url::Url,
+    credential: Option<Credential>,
+    name: String,
+    reference: String,
+    accept: Vec<String>,
 }
 
 impl Request {
@@ -44,12 +45,12 @@ impl Request {
         accept: Vec<String>,
     ) -> Self {
         Self {
-            _authentication: authentication,
-            _base_url: base_url,
-            _credential: credential,
-            _name: name,
-            _reference: reference,
-            _accept: accept,
+            authentication,
+            base_url,
+            credential,
+            name,
+            reference,
+            accept,
         }
     }
 }
@@ -58,15 +59,43 @@ impl distribution::Request for Request {
     type Future = Pin<Box<dyn Future<Output = Result<hyper::Request<hyper::Body>, error::Error>>>>;
 
     fn authentication(&self) -> Option<&Authentication> {
-        todo!()
+        self.authentication.as_ref()
     }
 
     fn credential(&self) -> Option<&Credential> {
-        todo!()
+        self.credential.as_ref()
     }
 
-    fn to_http_request(&self, _authentication: Option<&Authentication>) -> Self::Future {
-        todo!()
+    fn to_http_request(&self, authentication: Option<&Authentication>) -> Self::Future {
+        let mut base_url = self.base_url.clone();
+        base_url.set_path(&format!("/v2/{}/manifests/{}", self.name, self.reference));
+
+        let uri = base_url.to_string();
+
+        let mut request = hyper::Request::builder()
+            .method(hyper::Method::GET)
+            .uri(uri);
+
+        if !self.accept.is_empty() {
+            let accept = self.accept.join(",");
+            request = request.header("Accept", accept);
+        }
+
+        if let Some(authentication) = authentication.or(self.authentication.as_ref()) {
+            match authentication {
+                Authentication::Basic(token) => {
+                    request = request.header("Authorization", format!("Basic {token}"));
+                }
+                Authentication::Bearer(bearer) => {
+                    request =
+                        request.header("Authorization", format!("Bearer {}", bearer.access_token));
+                }
+            }
+        }
+
+        let result = request.body(hyper::body::Body::empty());
+
+        Box::pin(async move { result.map_err(Into::into) })
     }
 }
 
@@ -92,15 +121,24 @@ impl distribution::Response for Response {
     type Future = Pin<Box<dyn Future<Output = Result<Self, error::Error>>>>;
 
     fn new(
-        _http_response: hyper::Response<hyper::Body>,
-        _authentication: Option<Authentication>,
+        http_response: hyper::Response<hyper::Body>,
+        authentication: Option<Authentication>,
     ) -> Self::Future {
-        todo!()
+        let result = Ok(Self {
+            authentication,
+            http_response,
+        });
+        Box::pin(async move { result })
     }
 
     fn www_authenticate(
         &self,
     ) -> Result<Option<distribution::www_authenticate::WwwAuthenticate>, error::Error> {
-        todo!()
+        let Some(header) = self.http_response.headers().get("Www-Authenticate") else {
+            return Ok(None);
+        };
+        let value = header.to_str()?;
+        let www_authenticate = WwwAuthenticate::parse(value)?;
+        Ok(Some(www_authenticate))
     }
 }
